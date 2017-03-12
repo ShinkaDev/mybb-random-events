36be4af7dd9353c8d3891ccbea705230dd68e7ae
kalynrobinson <kalynrobinson@gmail.com>
2017-03-12 00:05:36 -0500

    Initial plugin file.

---
+++ inc/plugins/randomevents.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Dice Roller MyCode
+ * Copyright 2017 Shinka, All Rights Reserved
+ *
+ * License: http://www.mybb.com/about/license
+ *
+ */
+
+// Disallow direct access to this file for security reasons
+if(!defined("IN_MYBB")) {
+	die("Direct initialization of this file is not allowed.");
+}
+
+
+function randomevents_info() {
+	return array(
+		"name"			=> "Random Events",
+		"description"	=> "",
+		"website"		=> "",
+		"author"		=> "",
+		"authorsite"	=> "",
+		"version"		=> "1.0",
+		"guid" 			=> "",
+		"codename"		=> "",
+		"compatibility" => "*"
+	);
+}
+
+function randomevents_install()
+{
+
+}
+
+function randomevents_is_installed()
+{
+
+}
+
+function randomevents_uninstall()
+{
+
+}
+
+function randomevents_activate()
+{
+
+}
+
+function randomevents_deactivate()
+{
+
+}
